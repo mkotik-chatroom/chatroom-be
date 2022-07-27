178aa470da9e323d46be33f57aff7d819a55c88b
Marat Kotik <mkotik97@gmail.com>
2022-07-26 21:24:54 -0400

    test endpoint

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,13 @@ const server = express();
 const port = process.env.PORT || 8000;
 server.use(cors());
 server.get("/", (req, res) => {
-    res.send("Server works!");
+  res.send("Server works!");
+});
+
+server.get("/test", (req, res) => {
+  res.send("endpoint works");
 });
 server.listen(port, () => {
-    console.log(`server listening on port ${port}`);
+  console.log(`server listening on port ${port}`);
 });
 //# sourceMappingURL=index.js.map
