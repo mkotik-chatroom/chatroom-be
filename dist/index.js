e06426fbbc8b15b02bac101d5bda2b48361c1f17
Marat Kotik <mkotik97@gmail.com>
2022-07-26 23:04:05 -0400

    check port

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const server = express();
 const port = process.env.PORT || 8000;
+
 server.use(cors());
 server.get("/", (req, res) => {
   res.send("Server works!");
 });
 
 server.get("/test", (req, res) => {
-  res.send("endpoint works");
+  res.send(port.toString());
 });
 server.listen(port, () => {
   console.log(`server listening on port ${port}`);
