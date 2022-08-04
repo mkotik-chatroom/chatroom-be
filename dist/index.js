dc3b5aaf1639d7e7346dd4775dc58071b9e725f4
Marat Kotik <mkotik97@gmail.com>
2022-08-03 21:08:14 -0400

    Added build

---
+++ dist/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const port = process.env.PORT || 8000;
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        extraHeaders: {
+            "Access-Control-Allow-Credentials": "true",
+        },
+    },
+});
+app.get("/", (req, res) => {
+    res.send("server works!");
+});
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        socket.removeAllListeners();
+    });
+    socket.on("message", ({ name, message }) => {
+        io.emit("message", { name, message });
+    });
+    socket.on("typing", () => {
+        socket.broadcast.emit("typing");
+    });
+});
+server.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
+//# sourceMappingURL=index.js.map
